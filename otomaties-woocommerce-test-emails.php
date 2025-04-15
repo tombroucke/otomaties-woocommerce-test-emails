97a0bf942b3d8112ff3f4a7748c3d9ce8c9e7443
Tom Broucke <tom@tombroucke.be>
2025-04-15 10:06:42 +0200

    🔼 chore(bump): Bump version to 2.0.0

--- otomaties-woocommerce-test-emails.php
+++ otomaties-woocommerce-test-emails.php
@@ -8,7 +8,7 @@
  * License:           GPLv2 or later
  * License URI:       http://www.opensource.org/licenses/gpl-license.php
  * Domain Path:       /languages
- * Version:           1.4.0
+ * Version:           2.0.0
  *
  * @package         Woocommerce_Preview_Emails
  */
